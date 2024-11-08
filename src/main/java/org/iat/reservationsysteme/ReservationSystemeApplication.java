719ec93fe3bb88512ae07fb0c14b6e047c052392
IAttar <iattar@ugap.fr>
2024-11-08 09:17:30 +0100

    Initial setup with .gitignore

---
+++ src/main/java/org/iat/reservationsysteme/ReservationSystemeApplication.java
@@ -0,0 +1,13 @@
+package org.iat.reservationsysteme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservationSystemeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReservationSystemeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/iat/reservationsysteme/ReservationSystemeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.iat.reservationsysteme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
